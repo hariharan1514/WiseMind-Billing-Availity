e20ac162c6450caf8b015b5bc5b8e7802147266d
hariharan1514 <hariharan.g@asprcmsolutions.com>
2025-07-03 17:07:53 +0530

    Added login part

--- WiseMind Availity Portal Billing - Phase1.py
+++ WiseMind Availity Portal Billing - Phase1.py
@@ -61,37 +61,37 @@ else:
 
         driver.get("https://apps.availity.com/web/onboarding")  # Launching the router
 
-        # # send the username & password to the represented field
-        # try:
-        #     username_element = WebDriverWait(driver, 60).until(
-        #         EC.visibility_of_element_located((By.XPATH, "//input[@id='userId']")))
-        #     username_element.send_keys(username)
-        # except:
-        #     print("Login issue, Please login after some time.!")
-        #     sys.exit()
-        #
-        # password_element = WebDriverWait(driver, 30).until(
-        #     EC.visibility_of_element_located((By.XPATH, "//input[@id='password']")))
-        # password_element.send_keys(password)
-        #
-        # login_button_element = WebDriverWait(driver, 10).until(
-        #     EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Sign In']")))
-        # login_button_element.click()
-        #
-        # code = input("Please enter the code manually and hit enter here:")
-        # time.sleep(7)
-        # popupCheck = input(
-        #     "Please clear all pop content then hit enter if not pop up content then go ahead and hit enter:")
-        # time.sleep(7)
-        #
-        # try:
-        #     portalerror_element = WebDriverWait(driver, 10).until(
-        #         EC.presence_of_element_located((By.XPATH, "//div[@class='card-title']")))
-        #     print("Oops! Something went wrong. Please try logging in again later.")
-        #     driver.quit()
-        #     sys.exit()
-        # except:
-        #     pass
+        # send the username & password to the represented field
+        try:
+            username_element = WebDriverWait(driver, 60).until(
+                EC.visibility_of_element_located((By.XPATH, "//input[@id='userId']")))
+            username_element.send_keys(username)
+        except:
+            print("Login issue, Please login after some time.!")
+            sys.exit()
+
+        password_element = WebDriverWait(driver, 30).until(
+            EC.visibility_of_element_located((By.XPATH, "//input[@id='password']")))
+        password_element.send_keys(password)
+
+        login_button_element = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Sign In']")))
+        login_button_element.click()
+
+        code = input("Please enter the code manually and hit enter here:")
+        time.sleep(7)
+        popupCheck = input(
+            "Please clear all pop content then hit enter if not pop up content then go ahead and hit enter:")
+        time.sleep(7)
+
+        try:
+            portalerror_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//div[@class='card-title']")))
+            print("Oops! Something went wrong. Please try logging in again later.")
+            driver.quit()
+            sys.exit()
+        except:
+            pass
 
         claim_encounters_status_button = WebDriverWait(driver, 60).until(
                 EC.presence_of_element_located((By.XPATH, "//h3[normalize-space()='Claims & Encounters']")))
