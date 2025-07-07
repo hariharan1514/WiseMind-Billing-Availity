6b75a6b402f6b77ec7f3897119f88f2f1e1f7972
hariharan1514 <hariharan.g@asprcmsolutions.com>
2025-07-07 17:14:51 +0530

    Modification

--- WiseMind Availity Portal Billing - Phase2.py
+++ WiseMind Availity Portal Billing - Phase2.py
@@ -19,7 +19,7 @@ if not os.path.exists(bcbs_file_path):
     sys.exit()
 else:
     bcbs_billing_df = pd.read_excel(bcbs_file_path, sheet_name=0)
-    print(f"Staright Forward Data Row Count: {len(bcbs_billing_df)}")
+    print(f" Transaction ID Data Row Count: {len(bcbs_billing_df)}")
     ### Check if the Straightforward Billing file contains at least one data row. ###
     if bcbs_billing_df.shape[0] == 0:
         print("No BCBS Claims detected in today's run. Script completed successfully with no records to process. \n\nExiting gracefully as per expected behavior.")
@@ -256,110 +256,110 @@ else:
             if not found_data:
                 continue
 
-        if active_tab or claimTab_status != "Archived":
-            bcbs_sheet.cell(row=row, column=data_columns['Active/Archived']).value = "Active"
-            bcbs_billing_wb.save(bcbs_file_path)
-
-        elif archived_tab:
-            bcbs_sheet.cell(row=row, column=data_columns['Active/Archived']).value = "Archived"
-            bcbs_billing_wb.save(bcbs_file_path)
-
-        ### Navigate to ledger ###
-        ledger_btn_element = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH,"//a[@aria-label='Ledger']")))
-        ledger_btn_element.click()
-
-        openinvoice_btn_element = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH,"//a[@data-aqa='openInvoices']")))
-        openinvoice_btn_element.click()
-
-        time.sleep(2)
-
-        service_table_element = WebDriverWait(driver, 120).until(EC.presence_of_all_elements_located((By.XPATH,"(//table[contains(@class, 'k-grid-table')]//tr)")))
-        driver.execute_script("document.body.style.zoom= '50%'")
-        for tbl_row in range(1, len(service_table_element) +1):
-
-            service_tblrow_element = WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH,f"//table[@class='k-grid-table']//tr[{tbl_row}]//td[@data-aqa='number']//span//a")))
-            extracted_invoice_number = service_tblrow_element.text
-
-            if invoice_number == extracted_invoice_number :#and service_type == title_value
-                # dos_match = True
-                print(f"Invoice Number: ({invoice_number}) matched.")
-                service_tblrow_element.click()
-                invoice_footnote_element = WebDriverWait(driver, 120).until(
-                    EC.element_to_be_clickable((By.XPATH,"//div[@class='k8_io bco_aiz']//textarea")))
-                invoice_footnote_element.click()
-                invoice_footnote_element.send_keys(f"Claim submitted through availity portal under the transaction ID#{transaction_number}")
-                save_button_element = WebDriverWait(driver, 120).until(
-                    EC.element_to_be_clickable((By.XPATH,"(//button[@data-aqa='saveInvoice'])[1]")))
-                save_button_element.click()
-                bcbs_sheet.cell(row=row, column=data_columns['Transaction Status']).value = "Transaction Number updated"
-                print("Transaction Number Updated.!\n")
+            if active_tab or claimTab_status != "Archived":
+                bcbs_sheet.cell(row=row, column=data_columns['Active/Archived']).value = "Active"
                 bcbs_billing_wb.save(bcbs_file_path)
-                break
 
-                time.sleep(2)
+            elif archived_tab:
+                bcbs_sheet.cell(row=row, column=data_columns['Active/Archived']).value = "Archived"
+                bcbs_billing_wb.save(bcbs_file_path)
 
-        if archived_tab or claimTab_status == "Archived":
-            driver.get("https://wisemind71.theranest.com/clients")
-            for attempt in range(3):
-                try:
-                    tab_element = WebDriverWait(driver, 120).until(
-                        EC.presence_of_element_located((By.XPATH, "(//span[contains(text(), 'Active')])[1]")))
-                    tab_element.click()
-                    time.sleep(2)
+            ### Navigate to ledger ###
+            ledger_btn_element = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH,"//a[@aria-label='Ledger']")))
+            ledger_btn_element.click()
+
+            openinvoice_btn_element = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH,"//a[@data-aqa='openInvoices']")))
+            openinvoice_btn_element.click()
+
+            time.sleep(2)
+
+            service_table_element = WebDriverWait(driver, 120).until(EC.presence_of_all_elements_located((By.XPATH,"(//table[contains(@class, 'k-grid-table')]//tr)")))
+            driver.execute_script("document.body.style.zoom= '50%'")
+            for tbl_row in range(1, len(service_table_element) +1):
+
+                service_tblrow_element = WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH,f"//table[@class='k-grid-table']//tr[{tbl_row}]//td[@data-aqa='number']//span//a")))
+                extracted_invoice_number = service_tblrow_element.text
+
+                if invoice_number == extracted_invoice_number :#and service_type == title_value
+                    # dos_match = True
+                    print(f"Invoice Number: ({invoice_number}) matched.")
+                    service_tblrow_element.click()
+                    invoice_footnote_element = WebDriverWait(driver, 120).until(
+                        EC.element_to_be_clickable((By.XPATH,"//div[@class='k8_io bco_aiz']//textarea")))
+                    invoice_footnote_element.click()
+                    invoice_footnote_element.send_keys(f"Claim submitted through availity portal under the transaction ID#{transaction_number}")
+                    save_button_element = WebDriverWait(driver, 120).until(
+                        EC.element_to_be_clickable((By.XPATH,"(//button[@data-aqa='saveInvoice'])[1]")))
+                    save_button_element.click()
+                    bcbs_sheet.cell(row=row, column=data_columns['Transaction Status']).value = "Transaction Number updated"
+                    print("Transaction Number Updated.!\n")
+                    bcbs_billing_wb.save(bcbs_file_path)
                     break
-                except StaleElementReferenceException:
-                    time.sleep(1)
-                    continue
-            search_bar_element = WebDriverWait(driver, 120).until(
-                EC.visibility_of_element_located((By.XPATH, '//div[@data-aqa="inputFullName"]//input')))
-            search_bar_element.clear()
-            search_bar_element.send_keys(portal_client_name)
-            time.sleep(1)
-            try:
-                no_data_element = WebDriverWait(driver, 5).until(
-                    EC.visibility_of_element_located((By.XPATH, "//div[text()='No data available']")))
-                search_bar_element.clear()
-            except:
-                clientname_table_element = WebDriverWait(driver, 120).until(
-                    EC.visibility_of_all_elements_located(
-                        (By.XPATH, "//table[contains(@class, 'k-grid-table')]//tr")))
-                if len(clientname_table_element) > 1:
-                    for tbl_row in range(1, len(clientname_table_element) + 1):
+
+                    time.sleep(2)
+
+            if archived_tab or claimTab_status == "Archived":
+                driver.get("https://wisemind71.theranest.com/clients")
+                for attempt in range(3):
+                    try:
+                        tab_element = WebDriverWait(driver, 120).until(
+                            EC.presence_of_element_located((By.XPATH, "(//span[contains(text(), 'Active')])[1]")))
+                        tab_element.click()
                         time.sleep(2)
-                        for attempt in range(3):
-                            try:
-                                client_name_element = WebDriverWait(driver, 120).until(
-                                    EC.presence_of_element_located(
-                                        (By.XPATH, f"//table//tbody//tr[{tbl_row}]/td[2]/span/a")))
-                                time.sleep(1)
-                                client_name_element.click()
+                        break
+                    except StaleElementReferenceException:
+                        time.sleep(1)
+                        continue
+                search_bar_element = WebDriverWait(driver, 120).until(
+                    EC.visibility_of_element_located((By.XPATH, '//div[@data-aqa="inputFullName"]//input')))
+                search_bar_element.clear()
+                search_bar_element.send_keys(portal_client_name)
+                time.sleep(1)
+                try:
+                    no_data_element = WebDriverWait(driver, 5).until(
+                        EC.visibility_of_element_located((By.XPATH, "//div[text()='No data available']")))
+                    search_bar_element.clear()
+                except:
+                    clientname_table_element = WebDriverWait(driver, 120).until(
+                        EC.visibility_of_all_elements_located(
+                            (By.XPATH, "//table[contains(@class, 'k-grid-table')]//tr")))
+                    if len(clientname_table_element) > 1:
+                        for tbl_row in range(1, len(clientname_table_element) + 1):
+                            time.sleep(2)
+                            for attempt in range(3):
+                                try:
+                                    client_name_element = WebDriverWait(driver, 120).until(
+                                        EC.presence_of_element_located(
+                                            (By.XPATH, f"//table//tbody//tr[{tbl_row}]/td[2]/span/a")))
+                                    time.sleep(1)
+                                    client_name_element.click()
+                                    break
+                                except StaleElementReferenceException:
+                                    continue
+                            client_id_element = WebDriverWait(driver, 40).until(
+                                EC.visibility_of_element_located((By.XPATH, "//div[@data-aqa='ClientID']//div[2]")))
+                            client_id_element_number = client_id_element.text
+
+                            if client_id_number != client_id_element_number:
+                                driver.back()
+                            else:
+                                driver.back()
+                                archived_btn_element = WebDriverWait(driver, 30).until(EC.element_to_be_clickable(
+                                    (By.XPATH, f"(//button[@data-aqa='btnArchive'])[{tbl_row}]")))
+                                archived_btn_element.click()
+
+                                arcSubmit_element = WebDriverWait(driver, 30).until(
+                                    EC.element_to_be_clickable((By.XPATH, "//button[@data-aqa='btnSubmit']")))
+                                arcSubmit_element.click()
                                 break
-                            except StaleElementReferenceException:
-                                continue
-                        client_id_element = WebDriverWait(driver, 40).until(
-                            EC.visibility_of_element_located((By.XPATH, "//div[@data-aqa='ClientID']//div[2]")))
-                        client_id_element_number = client_id_element.text
+                    else:
+                        archived_btn_element = WebDriverWait(driver, 30).until(
+                            EC.element_to_be_clickable((By.XPATH, "(//button[@data-aqa='btnArchive'])[1]")))
+                        archived_btn_element.click()
 
-                        if client_id_number != client_id_element_number:
-                            driver.back()
-                        else:
-                            driver.back()
-                            archived_btn_element = WebDriverWait(driver, 30).until(EC.element_to_be_clickable(
-                                (By.XPATH, f"(//button[@data-aqa='btnArchive'])[{tbl_row}]")))
-                            archived_btn_element.click()
-
-                            arcSubmit_element = WebDriverWait(driver, 30).until(
-                                EC.element_to_be_clickable((By.XPATH, "//button[@data-aqa='btnSubmit']")))
-                            arcSubmit_element.click()
-                            break
-                else:
-                    archived_btn_element = WebDriverWait(driver, 30).until(
-                        EC.element_to_be_clickable((By.XPATH, "(//button[@data-aqa='btnArchive'])[1]")))
-                    archived_btn_element.click()
-
-                    arcSubmit_element = WebDriverWait(driver, 30).until(
-                        EC.element_to_be_clickable((By.XPATH, "//button[@data-aqa='btnSubmit']")))
-                    arcSubmit_element.click()
+                        arcSubmit_element = WebDriverWait(driver, 30).until(
+                            EC.element_to_be_clickable((By.XPATH, "//button[@data-aqa='btnSubmit']")))
+                        arcSubmit_element.click()
 
         time.sleep(2)
         driver.get("https://wisemind71.theranest.com/home/logout")
